004066befa87e277a9ba7eed651478a43a72ed49
ReliableContainer28 <150867092+ReliableContainer28@users.noreply.github.com>
2024-01-30 14:03:45 -0800

    Update utils.cpp
    If joystick circular, won't have to worry about if statement translation above.

--- src/utils.cpp
+++ src/utils.cpp
@@ -68,6 +68,11 @@ void differential_to_joy(float wl, float wr, float &joy_x_out, float &joy_y_out)
         testY = 1;
     }
 
+    /*
+    testX = cos(theta);
+    testY = sin(theta);
+    */
+
     // rounds values and multiplies by scaling value to get vector of proper length for inputs
     testX = scalingFactor*(round(testX*100000)/100000);
     testY = scalingFactor*(round(testY*100000)/100000);
